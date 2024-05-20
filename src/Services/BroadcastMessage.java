f67c2b0d5e4fd9265e4de544a45daaed65266d44
Joao-Biederman <joaobiedetman@gmail.com>
2024-05-20 11:49:06 -0300

    Finishing some server things and adding broadcast service

--- src/Services/BroadcastMessage.java
+++ src/Services/BroadcastMessage.java
@@ -3,24 +3,20 @@ package Services;
 import Util.DatabaseConfig;
 import Util.SocketSerialize;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.net.Socket;
 
 
 public class BroadcastMessage {
-    private DataSource ds = DatabaseConfig.getDataSource();
 
-    public void BroadcastMessage(String chat, String user, String message) {
-        String query = "SELECT * FROM chat_client WHERE chat = ?";
-        try (Connection connection = ds.getConnection();
+    public void broadcastMessage(String chat, String user, String message) {
+        String query = "SELECT * FROM CLIENT_ROOM WHERE chat = ?";
+        try (Connection connection = DatabaseConfig.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, chat);
             ResultSet rs = statement.executeQuery();
